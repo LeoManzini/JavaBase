b091c32184de594f186114df1b22dc3803dd9c39
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-11-22 15:12:32 -0300

    Adding more features to the bank class and starting to work on car class

---
+++ Car/MainCar.java
@@ -0,0 +1,20 @@
+package Car;
+
+class MainCar 
+{
+    public static void main(String args[])
+    {
+        Car myCar = new Car();
+        myCar.color = "Green";
+        myCar.model = "Gol";
+        myCar.currentSpeed = 0;
+        myCar.maxSpeed = 180;
+
+        // Turn the car on
+        myCar.turnOn();
+
+        // Acelerate the car
+        myCar.speedUp(20);
+        System.out.println(myCar.currentSpeed);
+    }   
+}
