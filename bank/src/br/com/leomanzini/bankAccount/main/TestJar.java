a8f2b16e491cd8d69627f8c7d3899a94103b9607
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-12-07 19:11:19 -0300

    I need to download a jar that create screens, because I don't have acess to the Caelum repository

---
+++ bank/src/br/com/leomanzini/bankAccount/main/TestJar.java
@@ -0,0 +1,11 @@
+package br.com.leomanzini.bankAccount.main;
+
+import br.com.caelum.javafx.api.main.OlaMundo;
+
+public class TestJar 
+{
+	public static void main(String args[])
+	{
+		OlaMundo.main(args);
+	}
+}
