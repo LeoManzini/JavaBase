6c8c5ffa2e8dbfe0df24ab562e13759389f31ada
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2020-12-12 13:01:32 -0300

    Adding the class to test the screen

---
+++ bank/src/br/com/leomanzini/bankAccount/main/TestAccount.java
@@ -0,0 +1,11 @@
+package br.com.leomanzini.bankAccount.main;
+
+import br.com.caelum.javafx.api.main.TelaDeContas;
+
+public class TestAccount
+{
+	public static void main(String args[])
+	{
+		TelaDeContas.main(args);
+	}
+}
