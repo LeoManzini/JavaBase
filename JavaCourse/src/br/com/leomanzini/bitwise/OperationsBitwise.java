081b97b0b5508a776282eb9072e8aa9780872f56
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-06-09 21:33:23 -0300

    Adding the bitwise

--- JavaCourse/src/br/com/leomanzini/bitwise/OperationsBitwise.java
+++ JavaCourse/src/br/com/leomanzini/bitwise/OperationsBitwise.java
@@ -1,8 +1,21 @@
 package br.com.leomanzini.bitwise;
 
+import java.util.Scanner;
+
 public class OperationsBitwise {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int mask = 0b100000;
+		int n = sc.nextInt();
+		
+		if((n & mask) != 0) {
+			System.out.println("6th bit is true (1)!");
+		} else {
+			System.out.println("6th bit is false (0)!");
+		}
+				
+		sc.close();
 	}
 }
