ea3655ef90a672ec584878447b2fdceb007ed7f7
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-06-17 19:42:42 -0300

    Finishing exercise

--- JavaCourse/src/br/com/leomanzini/enums/WorkerLevel.java
+++ JavaCourse/src/br/com/leomanzini/enums/WorkerLevel.java
@@ -2,16 +2,7 @@ package br.com.leomanzini.enums;
 
 public enum WorkerLevel {
 	
-	JUNIOR(1),
-	MID_LEVEL(2),
-	SENIOR(3);
-	
-	private int value;
-	
-	private WorkerLevel(int value) {
-	}
-	
-	public int getValue() {
-		return this.value;
-	}
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
 }
