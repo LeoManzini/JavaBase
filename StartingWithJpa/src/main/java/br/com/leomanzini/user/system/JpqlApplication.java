8ebada49fafa54ecf448a9a29fcc37a740034106
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-10-31 14:22:24 -0300

    Compose the query return

--- StartingWithJpa/src/main/java/br/com/leomanzini/user/system/JpqlApplication.java
+++ StartingWithJpa/src/main/java/br/com/leomanzini/user/system/JpqlApplication.java
@@ -2,6 +2,7 @@ package br.com.leomanzini.user.system;
 
 import java.util.List;
 
+import br.com.leomanzini.user.system.dto.SystemUserDTO;
 import br.com.leomanzini.user.system.model.Domain;
 import br.com.leomanzini.user.system.model.SystemUser;
 import jakarta.persistence.EntityManager;
@@ -21,26 +22,14 @@ public class JpqlApplication {
 //		selectFilteredQuery(entityManager);
 //		selectNoTypedQuery(entityManager);
 
-		choosingReturn(entityManager);
+//		choosingReturn(entityManager);
+		
+//		choosingMoreThenOneReturnAbstraction(entityManager);
+		
+		choosingMoreThenOneReturn(entityManager);
 
 		entityManager.close();
 		entityManagerFactory.close();
-	}
-
-	public static void choosingReturn(EntityManager entityManager) {
-		String consultQuery = "select user.domain from SystemUser user where user.id = 1";
-
-		TypedQuery<Domain> typedQuery = entityManager.createQuery(consultQuery, Domain.class);
-		Domain domain = typedQuery.getSingleResult();
-
-		System.out.println(domain);
-		
-		// Query JPQL sempre retorna o tipo do campo buscado na consulta
-		consultQuery = "select user.name from SystemUser user where user.id = 1";
-		TypedQuery<String> stringTypedQuery = entityManager.createQuery(consultQuery, String.class);
-		String userName = stringTypedQuery.getSingleResult();
-		
-		System.out.println(userName);
 	}
 
 	public static void selectQuery(EntityManager entityManager) {
@@ -71,5 +60,54 @@ public class JpqlApplication {
 		SystemUser user = (SystemUser) noTypedQuery.getSingleResult();
 
 		System.out.println(user);
+	}
+	
+	public static void choosingReturn(EntityManager entityManager) {
+		String consultQuery = "select user.domain from SystemUser user where user.id = 1";
+
+		TypedQuery<Domain> typedQuery = entityManager.createQuery(consultQuery, Domain.class);
+		Domain domain = typedQuery.getSingleResult();
+
+		System.out.println(domain);
+		
+		// Query JPQL sempre retorna o tipo do campo buscado na consulta
+		consultQuery = "select user.name from SystemUser user where user.id = 1";
+		TypedQuery<String> stringTypedQuery = entityManager.createQuery(consultQuery, String.class);
+		String userName = stringTypedQuery.getSingleResult();
+		
+		System.out.println(userName);
+	}
+	
+	public static void choosingMoreThenOneReturn(EntityManager entityManager) {
+		
+		/**
+		 * Como temos o tipo desse objeto SystemUserDTO mapeado com os atributos que queremos utilizar na query, 
+		 * instanciamos um novo objeto a cada chamada da query com o retorno desses atributos. Precisamos chamar o nome do pacote
+		 * para o JPQL nao se perder durante a execucao, mas o retorno da execao e uma lista da nossa classe DTO instanciada na query
+		 * 
+		 * A CLASSE DTO PRECISA TER UM CONSTRUTOR COM OS ATRIBUTOS QUE QUEREMOS UTILIZAR PARA PODERMOS INSTANCIAR A MESMA
+		 */
+		String query = "select new br.com.leomanzini.user.system.dto.SystemUserDTO(id, name, login) from SystemUser";
+		
+		TypedQuery<SystemUserDTO> specificReturn = entityManager.createQuery(query, SystemUserDTO.class);
+		List<SystemUserDTO> listSpecifcReturn = specificReturn.getResultList();
+		
+		listSpecifcReturn.forEach(listItem -> System.out.println(listItem));
+	}
+	
+	public static void choosingMoreThenOneReturnAbstraction(EntityManager entityManager) {
+		
+		String query = "select id, name, login from SystemUser";
+		
+		/**
+		 * Como nao tenho um tipo de objeto definido para esse retorno especificado na string da query, abstraimos e usamos
+		 * um array de objetos criado em tempo de execucao, sendo as propriedades desse objeto, as propriedades selecionadas como retorno da query
+		 * tendo essas variaveis, os mesmos tipos de retorno da nossa classe entidade, como essa abstracao nao cria uma classe como precisamos, mantemos
+		 * o codigo abstrato, usando o tipo Object []
+		 */
+		TypedQuery<Object []> arrayOfObjects = entityManager.createQuery(query, Object [].class);
+		List<Object []> arrayListOfObjects = arrayOfObjects.getResultList();
+		
+		arrayListOfObjects.forEach(arrayItem -> System.out.println(String.format("%s, %s, %s", arrayItem)));
 	}
 }
