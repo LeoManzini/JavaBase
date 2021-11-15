3352cc5a86c5859fd74bcd55567d303676954db0
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-15 16:34:23 -0300

    Named queries examples

--- StartingWithJpa/src/main/java/br/com/leomanzini/system/user/natives/NativesQueries.java
+++ StartingWithJpa/src/main/java/br/com/leomanzini/system/user/natives/NativesQueries.java
@@ -1,10 +1,58 @@
 package br.com.leomanzini.system.user.natives;
 
+import java.util.List;
+
+import br.com.leomanzini.system.user.natives.model.Users;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
 public class NativesQueries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("RESOURCE_LOCAL");
+		EntityManager entityManager = entityFactory.createEntityManager();
+		
+//		simpleConsults(entityManager);
+//		mapResults(entityManager);
+//		mapDtoResults(entityManager);
+		usingNamedNativeQuery(entityManager);
+		
+		entityManager.close();
+		entityFactory.close();
 	}
+	
+	public static void simpleConsults(EntityManager entityManager) {
+		
+        Query query = entityManager.createNativeQuery("select * from users", Users.class);
+        List<Users> resultlist = query.getResultList();
 
+        resultlist.stream().forEach(user -> System.out.println(user));
+	}
+	
+	public static void mapResults(EntityManager entityManager) {
+		
+        Query query = entityManager.createNativeQuery("select * from sis_users", "users-another-field-name");
+        List<Users> resultlist = query.getResultList();
+
+        resultlist.stream().forEach(user -> System.out.println(user));
+    }
+	
+	public static void mapDtoResults(EntityManager entityManager) {
+		
+        Query query = entityManager.createNativeQuery("select user_id, user_login, user_name from sis_users", "users-dto");
+        List<Users> resultlist = query.getResultList();
+
+        resultlist.stream().forEach(user -> System.out.println(user));
+    }
+	
+	public static void usingNamedNativeQuery(EntityManager entityManager) {
+		
+        Query query = entityManager.createNamedQuery("named-query-users");
+        List<Users> resultlist = query.getResultList();
+
+        resultlist.stream().forEach(user -> System.out.println(user));
+    }
 }
