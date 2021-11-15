7e00f166a7903c3e88ffef039e139ab365123350
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-15 16:48:19 -0300

    Starting work with procedures

---
+++ StartingWithJpa/src/main/java/br/com/leomanzini/system/user/procedures/ProceduresApplication.java
@@ -0,0 +1,10 @@
+package br.com.leomanzini.system.user.procedures;
+
+public class ProceduresApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
