ab058a51a646e1cfa9d08a78dddaf2488bf308a3
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-15 17:44:19 -0300

    Creating the data injection file

---
+++ StartingWithJpa/target/classes/META-INF/database/data-injection.sql
@@ -0,0 +1,3 @@
+insert into product (name, price) values ('Smartphone Xiaomi', 996.0);
+insert into product (name, price) values ('Smart TV 32', 883.0);
+insert into product (name, price) values ('Camera IP Sem Fio', 68.9);
