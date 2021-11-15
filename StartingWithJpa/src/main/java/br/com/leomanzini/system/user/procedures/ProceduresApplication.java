f5e4ec6718546bfda6b0585973af43b9632114fa
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-15 16:54:09 -0300

    Drawing the main class methods

--- StartingWithJpa/src/main/java/br/com/leomanzini/system/user/procedures/ProceduresApplication.java
+++ StartingWithJpa/src/main/java/br/com/leomanzini/system/user/procedures/ProceduresApplication.java
@@ -1,10 +1,33 @@
 package br.com.leomanzini.system.user.procedures;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class ProceduresApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("RESOURCE_LOCAL");
+		EntityManager entityManager = entityFactory.createEntityManager();
+		
+//		queriesWithProcedures(entityManager);
+//		researchsWithProcedures(entityManager);
+//		updatingWithProcedures(entityManager);
+		
+		entityManager.close();
+		entityFactory.close();
 	}
-
+	
+	public static void queriesWithProcedures(EntityManager entityManager) {
+		
+	}
+	
+	public static void researchsWithProcedures(EntityManager entityManager) {
+		
+	}
+	
+	public static void updatingWithProcedures(EntityManager entityManager) {
+		
+	}
 }
